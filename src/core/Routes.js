b819c27995908ff2623a735a78273e96a15b6530
Carolyn McNeillie <cmcneillie@theworkinggroup.ca>
2019-12-06 15:38:30 -0500

    Appled border box to * & wrapped all routes in <PageTemplate>

--- src/core/Routes.js
+++ src/core/Routes.js
@@ -11,18 +11,18 @@ import { PageTemplate } from './PageTemplate'
 // High-level routes for the application
 // Sub-routes should be nested within components
 export const Routes = () => (
-  <Switch>
-    <Route exact path='/'>
-      <HelloWorldClass cheeseburger="cheeseburger" />
-      <HelloWorldFunctional hotdog="hotdog bun" isEnabled>
-        This is a child thing.
-        <Accordion title="Click click!"><p>Hihihihi! Here I am!</p></Accordion>
-      </HelloWorldFunctional>
-    </Route>
-    <Route exact path='/search'>
-      <PageTemplate>
+  <PageTemplate>
+    <Switch>
+      <Route exact path='/'>
+        <HelloWorldClass cheeseburger="cheeseburger" />
+        <HelloWorldFunctional hotdog="hotdog bun" isEnabled>
+          This is a child thing.
+          <Accordion title="Click click!"><p>Hihihihi! Here I am!</p></Accordion>
+        </HelloWorldFunctional>
+      </Route>
+      <Route exact path='/search'>
         <SearchResults/>
-      </PageTemplate>
-    </Route>
-  </Switch>
+      </Route>
+    </Switch>
+  </PageTemplate>
 )
